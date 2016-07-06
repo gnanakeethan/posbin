b4b904edd33a6c5b3889beb9021cf3d6cfb408a6
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2016-07-06 18:16:42 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ DEST=ubuntu@52.71.69.255:/home/ubuntu/src/
 if [ "$SRC" != "" ] ; then
     echo "Syncing $SRC to $DEST"
 
-    /usr/local/bin/fswatch -r --latency=0.1 -0 $SRC | xargs -0 -n 1 -I{} /usr/local/bin/rsync -rav -S --force --delete -e "/usr/bin/ssh -i /Users/gnanakeethan/.ssh/id_aws_use.pem -o StrictHostKeyChecking=yes" --exclude="vendor" --exclude="node_modules" --exclude="bower_components"  --exclude ".idea" --exclude="storage" --exclude=".env" $SRC $DEST
+    /usr/local/bin/fswatch -r --latency=0.1 -0 $SRC | xargs -0 -n 1 -I{} /usr/local/bin/rsync -rav -S --force --delete -e "/usr/bin/ssh -i /Users/gnanakeethan/.ssh/id_aws_use.pem -o StrictHostKeyChecking=yes" --exclude="vendor" --exclude="node_modules" --exclude="bower_components"  --exclude ".idea" --exclude="storage" --exclude=".env" --exclude="*.tmp" $SRC $DEST
 
 
 fi
