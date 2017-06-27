37cdea16e206cb12993060f1a1f0df7f3e56c3ea
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-06-27 06:48:08 +0530

    Update: adding comments to main.go
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- main.go
+++ main.go
@@ -18,38 +18,46 @@ func init() {
 	}
 
 }
+
+// gC grabs the application configuration using beego method
 func gC(conf string) string {
 	return beego.AppConfig.String(conf)
 }
 
 func main() {
 
+	//Enables GZip functionality
 	beego.BConfig.EnableGzip = true
+	beego.BConfig.WebConfig.StaticExtensionsToGzip = []string{".css", ".js", ".html"}
 
+	//Listens on 0.0.0.0 instead of ::0
 	beego.BConfig.Listen.ListenTCP4 = true
+
+	//Configuring HTTPs Mode
 	beego.BConfig.Listen.EnableHTTP = false
 	beego.BConfig.Listen.EnableHTTPS = true
-
-	beego.BConfig.WebConfig.StaticExtensionsToGzip = []string{".css", ".js", ".html"}
 	beego.BConfig.Listen.HTTPSCertFile = gC("certfile")
 	beego.BConfig.Listen.HTTPSKeyFile = gC("certkey")
 
+	//Expose Swagger Directory
 	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	beego.BConfig.WebConfig.DirectoryIndex = true
 
+	//Static Path Configurations
 	beego.SetStaticPath("/service-worker.js", gC("publicdir")+"/service-worker.js")
 	beego.SetStaticPath("/manifest.json", "public/manifest.json")
 	beego.SetStaticPath("/index.html", gC("publicdir")+"/index.html")
 	beego.SetStaticPath("/", gC("publicdir")+"/index.html")
-
 	beego.BConfig.WebConfig.StaticDir["/bower_components"] = gC("publicdir") + "/bower_components"
 	beego.BConfig.WebConfig.StaticDir["/src"] = gC("publicdir") + "/src"
 
+	//CORS Filter for allowing requests to API
 	beego.InsertFilter("/v1/*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowAllOrigins: true,
 		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:    []string{"Origin", "Content-Type", "Authorization", "Access-Control-Allow-Origin"},
 		ExposeHeaders:   []string{"Content-Length", "Access-Control-Allow-Origin"},
 	}))
+
 	beego.Run()
 }
