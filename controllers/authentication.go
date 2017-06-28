189c71e5de7a95a17c1c6e1e826627d78a57d371
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-06-28 15:22:43 +0530

    update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- controllers/authentication.go
+++ controllers/authentication.go
@@ -13,13 +13,25 @@ type AuthenticationController struct {
 // URLMapping is used to map default routes of the controller
 func (c *AuthenticationController) URLMapping() {
 	c.Mapping("Index", c.Index)
-	//c.Mapping("AllBlock", c.AllBlock)
+	c.Mapping("Post", c.Post)
+	// c.Mapping("AllBlock", c.AllBlock)
 }
 
 // Index method is used to catch the root URL request
 //
 // @router / [get]
 func (c *AuthenticationController) Index() {
+	c.Data["json"] = "Success"
+	c.ServeJSON()
+}
+
+// Post method is used to send login requests
+//
+// @router / [post]
+func (c *AuthenticationController) Post() {
+
+	// email := c.GetString("email")
+	// password := c.GetString("password")
 	c.Data["json"] = "Success"
 	c.ServeJSON()
 }
