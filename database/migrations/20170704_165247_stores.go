e60076baf64cb8cee1af8057e77afb82b24eca53
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-07-06 05:45:23 +0530

    Update: productivity update 1
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- database/migrations/20170704_165247_stores.go
+++ database/migrations/20170704_165247_stores.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/migration"
 	"github.com/gnanakeethan/posbin/database/migrations/migrator"
 )
@@ -18,32 +15,26 @@ type Stores_20170704_165247 struct {
 func init() {
 	m := &Stores_20170704_165247{}
 	m.Created = "20170704_165247"
-	migration.Register("Stores_20170704_165247", m)
 	m.TableName = "stores"
 	m.Engine = "InnoDB"
 	m.Charset = "utf8"
 	m.PriCol("id").SetAuto(true).SetNullable(false).SetDataType("INT(10)").SetUnsigned(true)
-	m.PriCol("sec_id").SetNullable(false).SetDataType("INT(10)").SetUnsigned(true)
 	m.UniCol("name_unique", "name").SetDataType("VARCHAR(255)").SetNullable(true)
-	m.UniCol("name_unique", "cals").SetDataType("VARCHAR(255)").SetNullable(true)
 	m.NewCol("address").SetDataType("text").SetNullable(true)
 	m.NewCol("contact").SetDataType("tinytext").SetNullable(true)
+	migration.Register("Stores_20170704_165247", m)
 }
 
 // Run the migrations
 func (m *Stores_20170704_165247) Up() {
-	// use m.SQL("CREATE TABLE ...") to make schema update
 	m.ModifyType = "create"
 	sql := m.GetSQL()
-	beego.BeeLogger.Info(fmt.Sprint(sql))
 	m.SQL(sql)
 }
 
 // Reverse the migrations
 func (m *Stores_20170704_165247) Down() {
-	// use m.SQL("DROP TABLE ...") to reverse schema update
 	m.ModifyType = "delete"
 	sql := m.GetSQL()
-	beego.BeeLogger.Info(fmt.Sprint(sql))
 	m.SQL(sql)
 }
