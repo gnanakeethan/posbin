4bb58f42fcd80600c25320b743f9383b5dcfb172
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-07-13 21:44:04 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ database/migrations/20170713_201929_stores.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Stores_20170713_201929 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Stores_20170713_201929{}
+	m.Created = "20170713_201929"
+	m.ddlSpec()
+	migration.Register("Stores_20170713_201929", m)
+}
+
+/*
+	refer beego/migration/doc.go
+*/
+func (m *Stores_20170713_201929) ddlSpec() {
+	m.CreateTable("stores", "InnoDB", "utf8")
+	m.PriCol("id").SetAuto(true).SetNullable(false).SetDataType("INT(10)").SetUnsigned(true)
+	m.UniCol("name_unique", "name").SetDataType("VARCHAR(255)").SetNullable(true)
+	m.NewCol("address").SetDataType("text").SetNullable(true)
+	m.NewCol("contact").SetDataType("tinytext").SetNullable(true)
+}
