0f0809dbac0512ecb676a033367ece159a5b5c82
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-07-16 08:30:25 +0530

    Update: adding migrations
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- database/migrations/20170716_065940_users.go
+++ database/migrations/20170716_065940_users.go
@@ -23,6 +23,11 @@ func init() {
 func (m *Users_20170716_065940) ddlSpec() {
 	m.CreateTable("users", "InnoDB", "utf8")
 	m.PriCol("id").SetAuto(true).SetNullable(false).SetDataType("INT(10)").SetUnsigned(true)
-	m.NewCol("username").SetNullable(true).SetDataType("VARCHAR(255)").SetDefault("NULL")
+	m.UniCol("username", "username").SetNullable(true).SetDataType("VARCHAR(255)").SetDefault("NULL")
+	m.NewCol("email").SetNullable(true).SetDataType("VARCHAR(255)").SetDefault("NULL")
+	m.NewCol("password").SetNullable(true).SetDataType("VARCHAR(255)").SetDefault("NULL")
+	m.NewCol("remember_token").SetNullable(true).SetDataType("VARCHAR(255)").SetDefault("NULL")
+	m.NewCol("created_at").SetNullable(true).SetDataType("DATETIME").SetDefault("NULL")
+	m.NewCol("updated_at").SetNullable(true).SetDataType("DATETIME").SetDefault("NULL")
 
 }
---
+++ database/migrations/20170716_071411_roles.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Roles_20170716_071411 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Roles_20170716_071411{}
+	m.Created = "20170716_071411"
+	m.ddlSpec()
+	migration.Register("Roles_20170716_071411", m)
+}
+
+/*
+	refer beego/migration/doc.go
+*/
+func (m *Roles_20170716_071411) ddlSpec() {
+	m.CreateTable("roles", "InnoDB", "utf8")
+	m.PriCol("id").SetAuto(true).SetNullable(false).SetDataType("INT(10)").SetUnsigned(true)
+	m.NewCol("name").SetNullable(false).SetDataType("VARCHAR(255)")
+	m.NewCol("display_name").SetNullable(true).SetDataType("VARCHAR(255)")
+	m.NewCol("description").SetNullable(true).SetDataType("VARCHAR(255)")
+	m.NewCol("created_at").SetNullable(true).SetDataType("DATETIME").SetDefault("NULL")
+	m.NewCol("updated_at").SetNullable(true).SetDataType("DATETIME").SetDefault("NULL")
+
+}
