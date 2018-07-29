176612e95b276f5c528eae42366666875f7f32ae
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2018-07-29 21:26:17 +0530

    Update
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- primer/src/typings.d.ts
+++
@@ -1,8 +0,0 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
-declare module 'leaflet';
-declare module 'perfect-scrollbar';
-declare module 'screenfull';
